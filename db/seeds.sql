81c8cfa1e886c9f548dbe8d6ce81a184b16148b4
evandavies11 <evandavies11@gmail.com>
2019-09-12 14:04:07 -0500

    database added

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Triple Bacon');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('double double cheese');
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('BBQ style southern', true);
